402efda9dcdf20f4f94f097c47f0792bf9be8309
Yusuke Yoda <yusuke.yoda@givery.co.jp>
2023-03-30 14:19:26 +0900

    Refactor

--- modules/database/spanner/autoscaler/scheduler.tf
+++ modules/database/spanner/autoscaler/scheduler.tf
@@ -1,3 +1,4 @@
+# NOTES: これを実行するとプロジェクトのロケーションが決まってしまうので、事前に手動で個性したほうが良い。プロジェクトのロケーションは後から変更ができないので慎重にやるべき。
 # resource "google_app_engine_application" "app" {
 #   project     = var.project
 #   location_id = var.location
@@ -12,21 +13,21 @@ resource "google_cloud_scheduler_job" "poller_job" {
   pubsub_target {
     topic_name = google_pubsub_topic.poller_topic.id
 
-    data = base64encode(jsonencode([
-    merge({
-      "projectId" : "${var.project}",
-      "instanceId" : "${var.spanner_name}",
-      "scalerPubSubTopic" : "${google_pubsub_topic.scaler_topic.id}",
-      "units" : "PROCESSING_UNITS",
-      "minSize" : var.min_size,
-      "maxSize" : var.max_size,
-      "scalingMethod" : "LINEAR",
-      "stateDatabase" : {
-        "name" : "firestore",
-      }
-    },
-    {})
-  ]))
+    data = base64encode(jsonencode(
+      [
+        {
+          "projectId" : "${var.project}",
+          "instanceId" : "${var.spanner_name}",
+          "scalerPubSubTopic" : "${google_pubsub_topic.scaler_topic.id}",
+          "units" : "PROCESSING_UNITS",
+          "minSize" : var.min_size,
+          "maxSize" : var.max_size,
+          "scalingMethod" : "LINEAR",
+          "stateDatabase" : {
+            "name" : "firestore",
+          }
+        }
+    ]))
   }
 
   # depends_on = [google_app_engine_application.app]
