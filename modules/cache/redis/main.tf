62ed48c8ef0719c43adba76c5bcd1fb1c942a18c
Yusuke Yoda <yusuke.yoda@givery.co.jp>
2023-03-30 14:20:20 +0900

    Adjust network

--- modules/cache/redis/main.tf
+++ modules/cache/redis/main.tf
@@ -7,19 +7,34 @@ variable "region" {
   default = "asia-northeast1"
 }
 
-# resource "google_redis_instance" "default" {
-#   name               = var.name
-#   tier               = "STANDARD_HA"
-#   memory_size_gb     = 1
-#   authorized_network = "projects/my-project-id/global/networks/default"
-#   location_id        = "us-central1-a"
-#   redis_version      = "REDIS_5_0"
-# }
+resource "google_redis_instance" "cache" {
+  name           = "ha-memory-cache"
+  tier           = "STANDARD_HA"
+  memory_size_gb = 1
 
-# resource "google_redis_instance" "basic" {
-#   name           = var.name
-#   tier           = "BASIC"
-#   memory_size_gb = 2
-#   region         = var.region
-#   redis_version  = "REDIS_6_X"
-# }
+  location_id             = "asia-northeast1a"
+  alternative_location_id = "us-central1-f"
+
+  authorized_network = data.google_compute_network.redis-network.id
+
+  redis_version     = "REDIS_6_X"
+  display_name      = "Terraform Test Instance"
+  reserved_ip_range = "192.168.0.0/29"
+
+  labels = {
+    project    = "my_val"
+    other_key = "other_val"
+  }
+
+  maintenance_policy {
+    weekly_maintenance_window {
+      day = "TUESDAY"
+      start_time {
+        hours = 0
+        minutes = 30
+        seconds = 0
+        nanos = 0
+      }
+    }
+  }
+}
