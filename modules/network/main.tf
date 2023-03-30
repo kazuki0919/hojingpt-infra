1bf066acd043f7f8282e836d674afa5af2902611
Yusuke Yoda <yusuke.yoda@givery.co.jp>
2023-03-30 14:20:29 +0900

    Adjust network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -38,10 +38,14 @@ resource "google_compute_subnetwork" "public" {
   name          = "${var.name}-public${var.name_suffix}"
   ip_cidr_range = var.public_cidr_range
   network       = google_compute_network.default.name
+
+  private_ip_google_access = false
 }
 
 resource "google_compute_subnetwork" "private" {
   name          = "${var.name}-private${var.name_suffix}"
   ip_cidr_range = var.private_cidr_range
   network       = google_compute_network.default.name
+
+  private_ip_google_access = true
 }
