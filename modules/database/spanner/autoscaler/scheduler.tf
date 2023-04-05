1b4951a50a53a066e9aa4c99f15655ded11009a2
Yusuke Yoda <yusuke.yoda@givery.co.jp>
2023-04-05 15:42:32 +0900

    fine tune for autoscaler

--- modules/database/spanner/autoscaler/scheduler.tf
+++ modules/database/spanner/autoscaler/scheduler.tf
@@ -18,21 +18,26 @@ resource "google_cloud_scheduler_job" "poller_job" {
   pubsub_target {
     topic_name = google_pubsub_topic.poller_topic.id
 
-    data = base64encode(jsonencode(
-      [
-        {
-          "projectId" : "${var.project}",
-          "instanceId" : "${var.spanner_name}",
-          "scalerPubSubTopic" : "${google_pubsub_topic.scaler_topic.id}",
-          "units" : "PROCESSING_UNITS",
-          "minSize" : var.min_size,
-          "maxSize" : var.max_size,
-          "scalingMethod" : "LINEAR",
-          "stateDatabase" : {
-            "name" : "firestore",
+    # see: https://github.com/cloudspannerecosystem/autoscaler/blob/1ad8cb68d58b7d137853a0fe7e6b97c673f19492/poller/README.md#configuration-parameters
+    data = base64encode(
+      jsonencode(
+        [
+          {
+            "projectId" : "${var.project}",
+            "instanceId" : "${var.spanner_name}",
+            "scalerPubSubTopic" : "${google_pubsub_topic.scaler_topic.id}",
+            "units" : "PROCESSING_UNITS",
+            "minSize" : var.min_size,
+            "maxSize" : var.max_size,
+            "scaleInCoolingMinutes": 10,
+            "scalingMethod" : "STEPWISE",
+            "stateDatabase" : {
+              "name" : "firestore",
+            }
           }
-        }
-    ]))
+        ]
+      )
+    )
   }
 
   # depends_on = [google_app_engine_application.default]
