6de21146fea07732fb4a79ef6887e971c04b6364
Yusuke Yoda <yusuke.yoda@givery.co.jp>
2023-04-21 14:20:05 +0900

    Install redis-cli, mysql-client into bastion host

--- modules/bastion/startup.sh
+++ modules/bastion/startup.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get install -y zip unzip jq
+apt-get update && apt-get install -y zip unzip jq redis-tools mysql-client
