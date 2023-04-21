53ad5903541f1bd82e26f82d646908c51745eb8d
Yusuke Yoda <yusuke.yoda@givery.co.jp>
2023-04-21 20:06:10 +0900

    Add mysql log into log bucket

--- modules/logging/main.tf
+++ modules/logging/main.tf
@@ -20,21 +20,15 @@ variable "name_suffix" {
   default = ""
 }
 
+#######################################################################################
+# applog
+#######################################################################################
 resource "google_logging_project_bucket_config" "applog" {
   project          = var.project
   location         = var.location
   description      = "Application log"
   retention_days   = var.log_retention_days
   bucket_id        = "${var.name}-applog${var.name_suffix}"
-  enable_analytics = true
-}
-
-resource "google_logging_project_bucket_config" "accesslog" {
-  project          = var.project
-  location         = var.location
-  description      = "Access log"
-  retention_days   = var.log_retention_days
-  bucket_id        = "${var.name}-accesslog${var.name_suffix}"
   enable_analytics = true
 }
 
@@ -53,6 +47,18 @@ resource "google_logging_project_sink" "applog" {
   unique_writer_identity = true
 }
 
+#######################################################################################
+# accesslog
+#######################################################################################
+resource "google_logging_project_bucket_config" "accesslog" {
+  project          = var.project
+  location         = var.location
+  description      = "Access log"
+  retention_days   = var.log_retention_days
+  bucket_id        = "${var.name}-accesslog${var.name_suffix}"
+  enable_analytics = true
+}
+
 resource "google_logging_project_sink" "accesslog" {
   name        = "${var.name}-accesslog${var.name_suffix}"
   destination = "logging.googleapis.com/${google_logging_project_bucket_config.accesslog.name}"
@@ -60,6 +66,29 @@ resource "google_logging_project_sink" "accesslog" {
   filter = <<-EOT
     resource.type="cloud_run_revision"
     log_name="projects/${var.project}/logs/run.googleapis.com%2Frequests"
+  EOT
+
+  unique_writer_identity = true
+}
+
+#######################################################################################
+# mysql
+#######################################################################################
+resource "google_logging_project_bucket_config" "mysqllog" {
+  project          = var.project
+  location         = var.location
+  description      = "MySQL log"
+  retention_days   = var.log_retention_days
+  bucket_id        = "${var.name}-mysqllog${var.name_suffix}"
+  enable_analytics = true
+}
+
+resource "google_logging_project_sink" "mysqllog" {
+  name        = "${var.name}-mysqllog${var.name_suffix}"
+  destination = "logging.googleapis.com/${google_logging_project_bucket_config.mysqllog.name}"
+
+  filter = <<-EOT
+    resource.type="cloudsql_database"
   EOT
 
   unique_writer_identity = true
